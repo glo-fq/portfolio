09463a451447418a8f177545bb16b744950c18be
Gloriana <glorianafernandezq@hotmail.com>
2024-12-15 19:49:12 -0600

    chore: Removes unused components from Next page

--- apps/webpage/src/app/page.tsx
+++ apps/webpage/src/app/page.tsx
@@ -1,30 +1,6 @@
 'use client';
 
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  Button,
-  Card,
-  CardContent,
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-  Skeleton,
-  Typography,
-} from '@portfolio/ui-library';
+import { Typography } from '@portfolio/ui-library';
 
 export default function Home() {
   return (
@@ -39,76 +15,7 @@ export default function Home() {
         gap: '1rem',
       }}
     >
-      <Button>{'Hello world!'}</Button>
-      <Carousel style={{ width: '100%', maxWidth: '20rem' }}>
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <div style={{ padding: '0.25rem' }}>
-                <Card>
-                  <CardContent
-                    style={{
-                      display: 'flex',
-                      aspectRatio: '1/1',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      padding: '1.5rem',
-                    }}
-                  >
-                    <span style={{ fontSize: '2.25rem', fontWeight: '600' }}>
-                      {index + 1}
-                    </span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <div>
-          <CarouselPrevious />
-          <CarouselNext />
-        </div>
-      </Carousel>
-      <div>
-        <Alert>
-          <AlertTitle>Heads up!</AlertTitle>
-          <AlertDescription>
-            You can add components to your app using the cli.
-          </AlertDescription>
-        </Alert>
-      </div>
-      <Skeleton height="50px" width="50px" />
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href="#" />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" isActive>
-              2
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href="#" />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-      <Typography variant="h1" color="#69caca">
-        Hello world!
-      </Typography>
+      <Typography variant="h1">Work in progress</Typography>
     </div>
   );
 }
