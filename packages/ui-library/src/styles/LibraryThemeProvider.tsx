d226fb88ad20645fe3219c60f8903184d2853117
Gloriana <glorianafernandezq@hotmail.com>
2024-12-15 20:30:30 -0600

    feat: Adds theme prop for theme provider

--- packages/ui-library/src/styles/LibraryThemeProvider.tsx
+++ packages/ui-library/src/styles/LibraryThemeProvider.tsx
@@ -3,9 +3,10 @@ import { ThemeProvider, useTheme, DefaultTheme } from 'styled-components';
 import { defaultTheme } from './theme';
 import { GlobalStyles } from './GlobalStyles';
 
-export const LibraryThemeProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const LibraryThemeProvider: React.FC<{
+  children: React.ReactNode;
+  theme?: DefaultTheme;
+}> = ({ children, theme: propTheme }) => {
   let existingTheme: DefaultTheme | undefined;
 
   try {
@@ -16,7 +17,7 @@ export const LibraryThemeProvider: React.FC<{ children: React.ReactNode }> = ({
     existingTheme = undefined;
   }
 
-  const theme = existingTheme || defaultTheme;
+  const theme = propTheme ?? existingTheme ?? defaultTheme;
 
   return (
     <ThemeProvider theme={theme}>
